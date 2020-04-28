e85ecd7aa2dba1dd515e6dde1ffcab68024fdeda
qendev <qendev00@gmail.com>
2020-04-28 11:53:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationwithfragments_kotlin/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.bottomnavigationwithfragments_kotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import fragments.DashboardFragment
+import fragments.HomeFragment
+import fragments.NotificationsFragment
+import fragments.ProfileFragment
+
+class MainActivity : AppCompatActivity()  {
+
+
+
+    //create three objects for the fragments
+    lateinit var homeFragment: HomeFragment
+    lateinit var profileFragment: ProfileFragment
+    lateinit var dashboardFragment: DashboardFragment
+    lateinit var notificationsFragment: NotificationsFragment
+
+
+    lateinit var bottomNavigationView: BottomNavigationView
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+       // title=resources.getString(R.string.title_home)
+        loadFragment(HomeFragment())
+
+        bottomNavigationView = findViewById(R.id.navigation)
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {item ->
+            when(item.itemId){
+                R.id.navigation_home-> {
+                    title=resources.getString(R.string.title_home)
+                    loadFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.navigation_profile-> {
+                    title=resources.getString(R.string.title_profile)
+                    loadFragment(ProfileFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.navigation_dashboard-> {
+                    title=resources.getString(R.string.title_dashboard)
+                    loadFragment(DashboardFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.navigation_notifications-> {
+                    title=resources.getString(R.string.title_notifications)
+                    loadFragment(NotificationsFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+            }
+            false
+
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        // load fragment
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.layout_frame, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
